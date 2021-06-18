e8be00a53e4e0b4e907362ea93d288afb9e9cf18
ZhuoqunWang0120 <zw122@duke.edu>
2021-06-18 19:48:07 -0400

    updated run.sh

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # LTN_analysis
 LTN paper.
-Please set the working directory before running `run.sh`
+
+Please set the working directory before running `run.sh`.
 ```bash
-WORK_DIR=...
+export WORK_DIR=...
 ```
+To reproduce the results in the paper, run
+```bash
+./run.sh 100 
+```  
+
--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+cov_nsim=$1
 mkdir $WORK_DIR/LTN_analysis/results
 mkdir $WORK_DIR/LTN_analysis/cache
 $WORK_DIR/LTN_analysis/src/data/process_data_otu50.R $WORK_DIR
 $WORK_DIR/LTN_analysis/src/data/process_data_simulation.R $WORK_DIR
 $WORK_DIR/LTN_analysis/src/data/process_data_application.R $WORK_DIR
 $WORK_DIR/LTN_analysis/src/data/dtm_data.R $WORK_DIR
+$WORK_DIR/LTN_analysis/src/experiments/covariance_estimation/dtm_clrcov.R $WORK_DIR 1000000
+for i in `seq 1 $cov_nsim`; do
+    $WORK_DIR/LTN_analysis/src/experiments/covariance_estimation/dtm_sim.R $WORK_DIR $i
+done
