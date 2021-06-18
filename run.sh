b234215bc6d2e15a5156e7c48b8c4deb16f06a57
ZhuoqunWang0120 <zw122@duke.edu>
2021-06-18 15:19:32 -0400

    data process

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 mkdir $WORK_DIR/LTN_analysis/results
 mkdir $WORK_DIR/LTN_analysis/cache
+$WORK_DIR/LTN_analysis/src/data/process_data_otu50.R $WORK_DIR
+$WORK_DIR/LTN_analysis/src/data/process_data_simulation.R $WORK_DIR
 $WORK_DIR/LTN_analysis/src/data/process_data_application.R $WORK_DIR
